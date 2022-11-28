7af00d2e8299157660267b127ca87b433888e120
AJ Willsea <AJWillsea@rocketmortgage.com>
2022-11-28 16:21:33 -0500

    first commit

---
+++ Our E_F_C_P_E_F_ E Final Project/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Our_E_F_C_P_E_F__E_Final_Project
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
